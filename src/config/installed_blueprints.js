158bb0ac23cbbeccc0a0295d41414a6fb071a335
Crowdbotics <team@crowdbotics.com>
2020-10-05 23:59:23 +0000

    add package blueprint-react-native-emailauth-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'EmailAuth1128576', human_name: 'Email Auth 1', access_route: 'EmailAuth1128576', icon: 'envelope-o'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import EmailAuth1128576Navigator from '../features/EmailAuth1128576/navigator';
 
 /**
  * new navigators can be imported here
@@ -13,6 +14,7 @@ import SideMenu from './sideMenu';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+EmailAuth1128576: { screen: EmailAuth1128576Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
